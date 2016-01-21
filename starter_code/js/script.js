f640aa6d31c1c560cb69518e14ab281c32aac1e3
rofiqul <rofiqulmowla@mybihs.org>
2016-01-21 22:59:15 +0000

    flickr search

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,14 @@
 // YOUR CODE GOES HERE
+$(document).ready(function (){
+    
+    $("#search-btn").click(function(){
+    
+        var name=   $('#search-query').val()
+        console.log(name)
+        $("#reset").click(function(){
+            var name1=
+        })
+    })
+    
+})
+
